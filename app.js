0cc524b97956180c9ed81ec627ed091f5737c836
Bruno Jauad <brunojauad26@icloud.com>
2025-11-09 16:18:58 +0000

    allowing cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { getApi } = require("./controllers/api.controller");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 const {
